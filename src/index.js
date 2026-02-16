663e8ee1d0cfc50eaaa4b62a154073a74aed4740
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2026-02-16 17:04:51 -0600

    Complete multiple tests for capitalize function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./styles.css";
 
 function capitalize(string) {
+  if (string.length === 0) {
+    return "";
+  }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,27 @@
 import { capitalize } from "./index.js";
 
-test("capital test", () => {
-  expect(capitalize("bomb")).toBe("Bomb");
+describe("capitalize", () => {
+  test("regular lowercase string", () => {
+    expect(capitalize("sam")).toBe("Sam");
+  });
+
+  test("empty string", () => {
+    expect(capitalize("")).toBe("");
+  });
+
+  test("strings that start with numbers", () => {
+    expect(capitalize("241sarang")).toBe("241sarang");
+  });
+
+  test("strings that already start with uppercase", () => {
+    expect(capitalize("Bob")).toBe("Bob");
+  });
+
+  test("single character string", () => {
+    expect(capitalize("s")).toBe("S");
+  });
+
+  test("multiple words", () => {
+    expect(capitalize("snuggle muffin")).toBe("Snuggle muffin");
+  });
 });
